55d8a74e8a227584107fd32c44722eb6b9195108
dani <danielme_com@yahoo.com>
2021-06-22 19:47:59 +0200

    logging with slf4j

--- hello/src/main/java/com/danielme/jakartaee/hello/HelloServlet.java
+++ hello/src/main/java/com/danielme/jakartaee/hello/HelloServlet.java
@@ -5,12 +5,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/helloServlet")
 public class HelloServlet extends HttpServlet {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(HelloServlet.class);
 
     private static final String HELLO_MESSAGE = "Jakarta EE rocks!!";
 
@@ -19,6 +24,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         PrintWriter printWriter = response.getWriter();
         response.setContentType("text/plain;charset=UTF-8");
+        log.debug("returning the message {}", HELLO_MESSAGE);
         printWriter.print(HELLO_MESSAGE);
     }
 
