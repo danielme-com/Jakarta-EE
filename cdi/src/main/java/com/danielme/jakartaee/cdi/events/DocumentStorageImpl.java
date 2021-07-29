5c528bf70021db50cae74933ad9d4e31227a00e2
dani <danielme_com@yahoo.com>
2021-07-29 20:09:37 +0200

    use slf4j

--- cdi/src/main/java/com/danielme/jakartaee/cdi/events/DocumentStorageImpl.java
+++ cdi/src/main/java/com/danielme/jakartaee/cdi/events/DocumentStorageImpl.java
@@ -3,9 +3,14 @@ package com.danielme.jakartaee.cdi.events;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class DocumentStorageImpl implements DocumentStorage {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(DocumentStorageImpl.class);
 
     @Inject
     Event<DocumentSavedEvent> event;
@@ -14,7 +19,7 @@ public class DocumentStorageImpl implements DocumentStorage {
     public void add(Document document) {
         event.fire(new DocumentSavedEvent(document));
         event.fireAsync(new DocumentSavedEvent(document));
-        System.out.println("document saved");
+        log.info("document saved");
     }
 
 }
--- cdi/src/main/java/com/danielme/jakartaee/cdi/events/NotificationService.java
+++ cdi/src/main/java/com/danielme/jakartaee/cdi/events/NotificationService.java
@@ -5,11 +5,16 @@ import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.ObservesAsync;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @ApplicationScoped
 public class NotificationService {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(NotificationService.class);
 
     @Inject
     @Named("notifications")
@@ -17,17 +22,17 @@ public class NotificationService {
 
     void documentCreationObserver(@Observes DocumentSavedEvent documentSavedEvent) {
         notifications.add(Observes.class.getSimpleName() + documentSavedEvent.getDocument().getName());
-        System.out.println("notification sended");
+        log.info("notification sended");
     }
 
     void documentCreationObserverAsync(@ObservesAsync DocumentSavedEvent documentSavedEvent) {
         try {
             Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            log.error(ex.getMessage(), ex);
         }
         notifications.add(ObservesAsync.class.getSimpleName() + documentSavedEvent.getDocument().getName());
-        System.out.println("notification sended async");
+        log.info("notification sended async");
     }
 
 }
