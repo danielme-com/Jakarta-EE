cfbcf5da9f21d9fbe3790b7995bca705fc4dfa71
dani <danielme_com@yahoo.com>
2021-08-18 22:47:54 +0200

    equals and hascode

--- jpa-entity/src/main/java/com/danielme/jakartaee/jpa/entities/CityPK.java
+++ jpa-entity/src/main/java/com/danielme/jakartaee/jpa/entities/CityPK.java
@@ -3,14 +3,13 @@ package com.danielme.jakartaee.jpa.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -20,4 +19,18 @@ public class CityPK implements Serializable {
 
     @Column(name = "reg_code", length = 3)
     private String regionCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CityPK cityPK = (CityPK) o;
+        return name.equals(cityPK.name) && regionCode.equals(cityPK.regionCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, regionCode);
+    }
+
 }
